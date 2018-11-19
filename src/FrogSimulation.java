c32d0cff203cb9b20cd7062f14c94afe16fa7041
clam3154 <clam3154@bths.edu>
2018-11-19 09:38:27 -0500

    done

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -1,2 +1,45 @@
-public class FrogSimulation {
+public class FrogSimulation
+{
+
+    private int goalDistance;
+    private int maxHops;
+    public FrogSimulation(int dis, int numHops)
+    {
+        goalDistance = dis;
+        maxHops = numHops;
+    }
+    private int hopDistance()
+    {
+        int random = -4+(int)(Math.random()*20);
+        return random;
+    }
+    public boolean simulate()
+    {
+        int position = 0;
+        for (int count = 0; count < maxHops; count++)
+        {
+            position += hopDistance();
+            if (position >= goalDistance)
+            {
+                return true;
+            }
+            else if (position < 0)
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+    public double runSimulations(int num)
+    {
+        int countSuccess = 0;
+        for (int count = 0; count < num; count++)
+        {
+            if(simulate())
+            {
+                countSuccess++;
+            }
+        }
+        return (double)countSuccess / num;
+    }
 }
